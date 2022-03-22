51f5de8e25a23d24144a3cfcdcbb9608fe5ea608
Tiago Kaneta <tiago.kaneta@robovision.eu>
2022-03-22 15:26:38 +0100

    add sb

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { withSource } from './withSource'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  withSource
+]

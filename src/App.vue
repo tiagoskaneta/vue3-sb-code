2cd153139b54014047ae2fe0ebce76a094cbf98d
Tiago Kaneta <tiago.kaneta@robovision.eu>
2022-03-22 15:05:21 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Foobar msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Foobar from './components/Foobar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Foobar
   }
 }
 </script>
